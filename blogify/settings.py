0be2aa9fe688f1aaa800d6e866a6fd2012975bb5
jhosymijares <jhosy.mijares@gmail.com>
2022-08-09 13:42:46 +0000

    cambios

--- blogify/settings.py
+++ blogify/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'blogify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',                     
+        'USER': 'myuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
